e087997cb74dc6136195ccbd66406f0da0565ecc
Abdul <abdulwaheed18@gmail.com>
2016-09-01 15:54:23 +0530

    Added the contents of LICENSE file as comment in each and every file.

--- src/main/java/com/gslab/otp/AESEncryptor.java
+++ src/main/java/com/gslab/otp/AESEncryptor.java
@@ -1,5 +1,31 @@
-/**
- * Copyright 2014 GSLAB. All Rights Reserved.
+/*
+ * Copyright (c) 2016, Great Software Laboratory Private Limited.
+ *  All rights reserved.
+ *
+ * Contributor: Abdul Waheed [abdul.waheed@gslab.com]
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the "Great Software Laboratory Private Limited" nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package com.gslab.otp;
 
@@ -24,8 +50,7 @@ public class AESEncryptor implements IEncryptor {
 	/**
 	 * The logger for this class
 	 */
-	private static final Logger LOG = LoggerFactory
-			.getLogger(AESEncryptor.class);
+	private static final Logger LOG = LoggerFactory.getLogger(AESEncryptor.class);
 
 	public AESEncryptor() {
 		encrytor = new StandardPBEStringEncryptor();
@@ -45,17 +70,14 @@ public class AESEncryptor implements IEncryptor {
 		LOG.info("Encrypting shared Secret...");
 		if (sharedSecret == null || sharedSecret.isEmpty()) {
 			LOG.error("SharedSecret cannot be null or empty");
-			throw new OTPManagerException(
-					"SharedSecret cannot be null or empty");
+			throw new OTPManagerException("SharedSecret cannot be null or empty");
 		}
 		String encryptedKey = null;
 		try {
 			encryptedKey = encrytor.encrypt(sharedSecret);
 		} catch (Exception e) {
-			LOG.error("Error while encrypting sharedSecret {}", e.getMessage(),
-					e);
-			throw new OTPManagerException(
-					"Error while encrypting sharedSecret " + e.getMessage(), e);
+			LOG.error("Error while encrypting sharedSecret {}", e.getMessage(), e);
+			throw new OTPManagerException("Error while encrypting sharedSecret " + e.getMessage(), e);
 		}
 		LOG.debug("Encrypted Shared Secret successfully");
 		return encryptedKey;
@@ -71,17 +93,14 @@ public class AESEncryptor implements IEncryptor {
 		String decryptedKey = null;
 		if (sharedSecret == null || sharedSecret.isEmpty()) {
 			LOG.error("SharedSecret cannot be null or empty");
-			throw new OTPManagerException(
-					"SharedSecret cannot be null or empty");
+			throw new OTPManagerException("SharedSecret cannot be null or empty");
 		}
 		try {
 			decryptedKey = encrytor.decrypt(sharedSecret);
 		} catch (Exception e) {
 			// for Invalid sharedSecret, e.getMessage will be null
-			LOG.error("Error while decrypting sharedSecret {}", e.getMessage(),
-					e);
-			throw new OTPManagerException(
-					"Error while decrypting sharedSecret " + e.getMessage(), e);
+			LOG.error("Error while decrypting sharedSecret {}", e.getMessage(), e);
+			throw new OTPManagerException("Error while decrypting sharedSecret " + e.getMessage(), e);
 		}
 		LOG.debug("Decrypted Shared Secret successfully");
 		return decryptedKey;
